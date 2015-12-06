c1d7cbaeea3765fda94598e6ea264b8fc476849d
root <root@ip-172-31-3-168.ec2.internal>
2015-12-06 16:14:56 +0000

    updated refresh permissions script

---
+++ scripts/refresh-permissions.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+cd /var/www/html
+
+echo "chown"
+chown -R root *
+chgrp -R www *
+
+echo "files"
+find wordpress wp-admin wpclone_backup wp-includes scripts wp-content -type f -not \( -path "*/\.git/*" \) -exec chmod 664 {} \;
+echo "dirs"
+find wordpress wp-admin wpclone_backup wp-includes scripts wp-content -type d -not \( -path "*/\.git/*" \) -exec chmod 775 {} \;
+echo "scripts"
+chmod 770 scripts/*sh
+echo "done"
