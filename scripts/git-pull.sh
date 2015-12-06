330f4525973a2835a659e3b570b557e8e94f61f7
root <root@ip-172-31-3-168.ec2.internal>
2015-12-06 21:57:32 +0000

    updated git pull file to peform pull each time

--- scripts/git-pull.sh
+++ scripts/git-pull.sh
@@ -6,12 +6,13 @@ LOCAL=$(git rev-parse @)
 REMOTE=$(git rev-parse @{u})
 BASE=$(git merge-base @ @{u})
 
+#Perform git pull
+git pull origin master && /var/www/html/scripts/refresh-permissions.sh
+
 if [ "$LOCAL" = "$REMOTE" ]; then
     echo "Up-to-date"
 elif [ "$LOCAL" = "$BASE" ]; then
     echo "Need to pull"
-    git pull origin master && /var/www/html/scripts/refresh-permissions.sh
-    
 elif [ "$REMOTE" = "$BASE" ]; then
     echo "Need to push"
 else
