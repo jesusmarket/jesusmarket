b40aa4ff4db4ca3222b6e0e87e8de42f80793014
root <root@ip-172-31-55-242.ec2.internal>
2016-02-03 22:06:29 +0000

    created backup script for ec2-user cron jobs that will automaticall backup cron

---
+++ scripts/cron-backup.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+cd /var/www/html/scripts
+
+COMMIT_MESSAGE="updated cron backup"
+
+#Check project is up to date from origin master before attempting to commit new changes
+git pull origin master && /var/www/html/scripts/refresh-permissions.sh
+
+#Copy ec2-user crontab into a temp file
+crontab -l -u ec2-user > cron.tmp
+
+#Copy into the cron.md version controlled backup file and remove temp
+cp cron.tmp cron.md && rm cron.tmp
+
+#Perform a git add of cron.md file if any changes have been made
+git add cron.md
+echo "adding new files to git"
+
+git status
+
+#Commit change with message added
+git commit -m $COMMIT_MESSAGE
+echo "commiting new changes with message: $COMMIT_MESSAGE"
+
+#Push changes out to origin master
+git push origin master
+
+echo "Finished commiting new changes"
+
+exit 0
